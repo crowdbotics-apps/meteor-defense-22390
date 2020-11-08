86e68eda16d7d0d6d38b2ed4800748b5e376ef0f
Crowdbotics <team@crowdbotics.com>
2020-11-08 01:52:16 +0000

    Initial commit.

---
+++ ios/meteor_defense_22390Tests/meteor_defense_22390Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface meteor_defense_22390 : XCTestCase
+
+@end
+
+@implementation meteor_defense_22390
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
